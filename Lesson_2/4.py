c494cb0b1e7e4b50f03aceec8bad83bdd6261090
Andrey Zakamaldin <zakamaldin.andrey@gmail.com>
2019-03-31 12:26:27 +0300

    lesson02

--- Lesson_2/4.py
+++ Lesson_2/4.py
@@ -2,3 +2,15 @@
 4.	Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
 Количество элементов (n) вводится с клавиатуры.
 """
+
+
+def count(base, num):
+    if num == 0:
+        return base
+    else:
+        print(base)
+        return base + count(base / (-2), num - 1)
+
+
+num = int(input('Введите натуральное число:'))
+print(count(1, num))
