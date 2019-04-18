c904cd36d0090b72b3848eb82a5862fb0c5f8d6a
Andrey Zakamaldin <zakamaldin.andrey@gmail.com>
2019-04-18 23:46:22 +0300

    lesson07

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,41 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+import random
+
+def mergeSort(alist):
+    if len(alist) > 1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i]<righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i<len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j<len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+
+numbers = [random.random()*50 for i in range(100)]
+print(numbers)
+mergeSort(numbers)
+print(numbers)
